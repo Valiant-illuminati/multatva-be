08d9d5230c0b8771eb537847cea6888196aac6af
Valiant-illuminati <varsha.valiant@gmail.com>
2025-06-02 15:18:09 +0530

    Add files via upload

---
+++ multatva/src/main/java/com/ekart/multatva/MultatvaApplication.java
@@ -0,0 +1,13 @@
+package com.ekart.multatva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultatvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultatvaApplication.class, args);
+	}
+
+}
---
+++ multatva/src/test/java/com/ekart/multatva/MultatvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekart.multatva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultatvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
